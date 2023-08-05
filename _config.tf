ab71d296c5b7f401e762954966ca33cd5ea51b62
umaruchan268 <84457632+umaruchan268@users.noreply.github.com>
2023-08-05 22:38:51 +0700

    Add files via upload

---
+++ _config.tf
@@ -0,0 +1,16 @@
+terraform {
+   backend "s3" {
+     bucket = "mybucketuyen"
+     key    = "terraform/terraform.tfstate"
+     region = "us-east-2"
+   }
+ }
+
+ provider "aws" {
+   region = "us-east-2"
+   #profile = "default"
+   
+   default_tags {
+     tags = local.tags
+   }
+ }
